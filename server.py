d72a4c3a98b10c2eed846f70a37bd0589523e0c2
Noah Reifsnyder <noahreifsnyder@gmail.com>
2019-10-04 16:40:16 -0400

    Updtaes

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 from socket import *
 import _thread as thread
+MSGID = 0
+
+
+def get_id():
+    global MSGID
+    id = MSGID
+    MSGID += 1
+    return id
 
 
 def start_up():
@@ -10,8 +18,7 @@ def start_up():
     s.listen(0)
     client,  address = s.accept()
     data = "hello"
-    client.send(data.encode('utf-8'))
-    client.close()
+    send_message(client, "Hello")
     s.close()
 
 
@@ -21,15 +28,26 @@ def get_ip_address():  # using google to obtain real ip, google most reliable ho
     return s.getsockname()[0]
 
 
-def listen(s):
-    while True:
-        l = int_from_bytes(s.recv(1))
-        emsg = s.recv(l)
-        msg=emsg.decode('utf-8')
-        if len(msg)>0:
-            thread.start_new_thread(parse,(msg,s,))
-        else:
-            pass
+#128, first 16 are info
+def send_message(s, message):
+    data = message.encode('utf-8')
+    length = (len(data))
+    messages = length // 128
+    id = get_id()
+    info = int_to_bytes(id) + int_to_bytes(messages)
+    for i in range(messages):
+        i_info = info + int_to_bytes(i)
+        data = i_info + message.encode('utf-8')
+        s.send(data)
+    return
+
+
+def int_to_bytes(x: int) -> bytes:
+    return x.to_bytes((x.bit_length() + 7) // 8, 'big')
+
+
+def int_from_bytes(xbytes: bytes) -> int:
+    return int.from_bytes(xbytes, 'big')
 
 
 start_up()
