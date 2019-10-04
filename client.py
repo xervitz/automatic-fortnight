d9daa04feebdb78b9a63df38e31b51c2605de56b
Miles Necker <mtn217@lehigh.edu>
2019-10-04 15:45:23 -0600

    Larger messages

--- client.py
+++ client.py
@@ -8,7 +8,28 @@ class ReceiveMessages:
         s = socket.socket()
         host = "72.79.65.14"
         port = 697
+        messDic = {}
+
         s.connect((host, port))
-        mess = s.recv(1024)
-        ParseMessages.Parse(mess)
+
+        mess = s.recv(4)
+
+        messInfo = []
+        for c in mess:
+            messInfo.append(c)
+
+        message = [None] * messInfo[1]
+        messDic[messInfo[0]] = message
+
+        while message.__contains__(None):
+            mess = s.recv(messInfo[3])
+            messDic[messInfo[0]][messInfo[2]] = mess
+            mess = s.recv(4)
+            messInfo = []
+            for c in mess:
+                messInfo.append(c)
+
+        for m in messDic[1]:
+            print(m)
+
         s.close()
