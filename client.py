344d40533d5d4cedc473e4b770f1b0e2a2ff9c89
Miles Necker <mtn217@lehigh.edu>
2019-10-04 14:21:17 -0600

    Wrote things for noah

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
 import socket
 
-class RecieveMessages:
-	def 
-	s = socket.socket()
-	host = "72.79.65.14"
-	port = 697
-	recievedMessage = []
+class ReceiveMessages:
+	def WaitForMessage(self):
+		s = socket.socket()
+		host = "72.79.65.14"
+		port = 697
+		receivedMessage = []
 
-	while True:
-		s.connect((host, port))
-		recievedMessage.append(s.accept(1024))
-		s.close()
+		while True:
+			s.connect((host, port))
+			receivedMessage.append(s.accept(1024))
+			s.close()
